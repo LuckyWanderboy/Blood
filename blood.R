e987c16dd0e8407f461cc1392ad6e82b453fa957
unknown <ganlaiwang@gmail.com>
2015-07-13 21:27:31 -0700

    d

--- blood.R
+++ blood.R
@@ -1,1 +1,13 @@
-testInheritedMethods()
+
+
+blood1 = read.csv("blood.csv")
+
+
+
+as.factor(blood1$Donated)
+blood1$Donated = as.factor(blood1$Donated)
+str(blood1)
+g = ggplot(blood1, aes(Donated, VolumeBlood))
+g + geom_boxplot()
+
+g = ggplot(blood1, aes(Donated, LastDonation))
